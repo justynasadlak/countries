62b6e38ee4985c1f24220f5bc2afb1133b35187d
Unknown <justynasadlak10@gmail.com>
2019-08-27 11:16:26 +0200

    Improve displaying country name

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Country } from '../../models/country';
 import { CountryDataService } from '../../services/country-data.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { DataToSend } from '../../models/data-to-send';
 
 @Component({
   selector: 'app-register-form',
@@ -19,18 +20,31 @@ export class RegisterFormComponent implements OnInit {
 
   ngOnInit(): void {
     this.initCountries();
+    this.initForm();
+  }
+
+  onSubmit(): void {
+    const dataToSend: DataToSend = {
+      firstName: this.registerForm.value.firstName,
+      lastName: this.registerForm.value.lastName,
+      alpha2Code: this.registerForm.value.country.alpha2Code
+    };
+    console.log(dataToSend);
+  }
+
+  displayCountryName(country: Country): string | Country {
+    return country ? country.name : country;
+  }
+
+  private initCountries(): void {
+    this.countryDataService.getAllCountries().subscribe(res => this.countries = res);
+  }
+
+  private initForm(): void {
     this.registerForm = this.formBuilder.group({
       firstName: [''],
       lastName: [''],
       country: ['']
     });
-  }
-
-  onSubmit(): void {
-    console.log(this.registerForm.value);
-  }
-
-  private initCountries(): void {
-    this.countryDataService.getAllCountries().subscribe(res => this.countries = res);
   }
 }
